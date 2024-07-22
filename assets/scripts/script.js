e868b35e450753ad51dd44171cac0b8141952764
pal-oe <fenanyosef@gmail.com>
2024-07-22 22:40:30 +0300

    inprogress

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -15,9 +15,11 @@ const items = [
   "Keyboard",
 ];
 let currentIndex = 0;
+let counter = 0;
 const currentImage = document.getElementById("current-image");
 const nextImage = document.getElementById("next-image");
 const nextItem = document.getElementById("next-item");
+const winner = document.getElementById("spinner-modal-winner");
 let directory = "./assets/images/";
 var audio = document.getElementById("audioPlayer");
 
@@ -32,9 +34,14 @@ function updateBalance(amount) {
   let currentBalance = parseFloat(balanceElement.textContent.replace("$", ""));
   currentBalance += amount;
   balanceElement.textContent = ` $${currentBalance.toFixed(2)}`;
+  counter += 1;
+  // if (currentBalance >= 800) {
+  //   showModal();
+  //   console.log(true);
+  // }
 
-  if (currentBalance >= 800) {
-    showModal();
+  if (counter == 5) {
+    showModal(currentBalance);
   }
 }
 
@@ -85,11 +92,76 @@ document.addEventListener("DOMContentLoaded", () => {
   showSection("tasks");
   currentImage.src = directory + images[currentIndex];
   nextItem.innerText = items[currentIndex];
+
+  const playButton = document.getElementById("play-button");
+  const wheel = document.getElementById("wheel");
+  const modal = document.getElementById("spinner-modal");
+
+  modal.style.display = "flex";
+
+  playButton.addEventListener("click", () => {
+    const fixedDegree = 3600 + 80;
+
+    wheel.style.transform = `rotate(${fixedDegree}deg)`;
+    // Generate a random degree between 360 and 3600 (10 full rotations + random)
+    // const randomDegree = Math.floor(Math.random() * 360) + 3600;
+
+    // // Spin the wheel
+    // wheel.style.transform = `rotate(${randomDegree}deg)`;
+
+    // // Calculate the prize index (0-9)
+    // const prizeIndex = Math.floor((randomDegree % 360) / 36);
+
+    // // Display the winner after the spin animation completes
+    setTimeout(() => {
+      const prizeSegments = wheel.getElementsByClassName("segment");
+      // const prize = prizeSegments[prizeIndex].textContent.trim();
+      // alert(`Congratulations! You won: ${prize}`);
+      modal.style.display = "none";
+      winner.style.display = "flex";
+    }, 4000); // 4 seconds to match the CSS transition duration
+  });
+
+  const buttons = document.querySelectorAll(".firework-button");
+  const fireworkContainer = document.getElementById("firework-container");
+  console.log("buttons", buttons);
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      // Create firework elements
+      for (let i = 0; i < 20; i++) {
+        const firework = document.createElement("div");
+        firework.className = "firework";
+        firework.style.width = `${Math.random() * 50 + 5}px`;
+        firework.style.height = firework.style.width;
+        firework.style.left = `${e.clientX - fireworkContainer.offsetLeft}px`;
+        firework.style.top = `${e.clientY - fireworkContainer.offsetTop}px`;
+        firework.style.transform = `translate(-50%, -50%) rotate(${
+          Math.random() * 360
+        }deg)`;
+        firework.style.backgroundColor = `rgba(${Math.random() * 255}, ${
+          Math.random() * 255
+        }, ${Math.random() * 255}, 0.8)`;
+        fireworkContainer.appendChild(firework);
+
+        // Remove firework element after animation
+        firework.addEventListener("animationend", () => {
+          firework.remove();
+        });
+      }
+    });
+  });
 });
 
+// to close all modals
+function closeModals() {
+  winner.style.display = "none";
+}
+
 // Show modal
-function showModal() {
+function showModal(currentBalance) {
   document.getElementById("withdrawalModal").style.display = "block";
+  document.getElementById("money-amount").innerText = "$" + currentBalance;
 }
 
 // Close modal
